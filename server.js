9e8f474dfaf8e1c1461c144aa41eb66724f0fdc6
PokeClub-Dean-Ray <raychung8484@gmail.com>
2025-07-19 17:44:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-// Serve chat.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'chat.html'));
-});
+const PORT = process.env.PORT || 3000;
 
-// Socket.io handling
+// Serve the static frontend files (like chat.html)
+app.use(express.static('.'));
+
+// Handle socket connections
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('A user connected');
 
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    // Broadcast the message to everyone except sender
+    socket.broadcast.emit('chat message', msg);
   });
 
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log('User disconnected');
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+http.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
